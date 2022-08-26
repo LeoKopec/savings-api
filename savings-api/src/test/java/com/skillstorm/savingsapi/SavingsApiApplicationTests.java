16df4b89fd8c705f509b7db41c367298b7e874aa
Leo-Kopec <lkopec4@gmail.com>
2022-08-26 17:13:11 -0400

    Initialized Project

---
+++ savings-api/src/test/java/com/skillstorm/savingsapi/SavingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.savingsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
