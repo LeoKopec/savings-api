2c75bb0ae2628351ff007572ada7bcf3cf613c24
Leo-Kopec <lkopec4@gmail.com>
2022-08-29 13:29:19 -0400

    Created Goal Controller

---
+++ savings-api/src/main/java/com/skillstorm/savingsapi/controllers/GoalController.java
@@ -0,0 +1,10 @@
+package com.skillstorm.savingsapi.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/goals")
+public class GoalController {
+
+}
