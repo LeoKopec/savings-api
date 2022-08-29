7e484122f04718bb89b562389a5ef6175b410fac
Leo-Kopec <lkopec4@gmail.com>
2022-08-29 10:53:51 -0400

    Created User and Goal Model

---
+++ savings-api/src/main/java/com/skillstorm/savingsapi/models/Goal.java
@@ -0,0 +1,142 @@
+package com.skillstorm.savingsapi.models;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "goals")
+public class Goal {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "image")
+	private String image;
+	
+	@Column(name = "date")
+	private LocalDate date;
+	
+	@Column(name = "currentAmount")
+	private double currentAmount;
+	
+	@Column(name = "totalAmount")
+	private double totalAmount;
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name = "userID")
+	@JsonBackReference
+	private User user;
+
+	// no-argument constructor
+	public Goal() {
+		super();
+	}
+
+	// constructor without the auto-incremented id
+	public Goal(String name, String description, String image, LocalDate date, double currentAmount, double totalAmount,
+			User user) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.date = date;
+		this.currentAmount = currentAmount;
+		this.totalAmount = totalAmount;
+		this.user = user;
+	}
+
+	// full-argument constructor
+	public Goal(int id, String name, String description, String image, LocalDate date, double currentAmount,
+			double totalAmount, User user) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.date = date;
+		this.currentAmount = currentAmount;
+		this.totalAmount = totalAmount;
+		this.user = user;
+	}
+
+	// getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public double getCurrentAmount() {
+		return currentAmount;
+	}
+
+	public void setCurrentAmount(double currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	// to-string function
+	@Override
+	public String toString() {
+		return "Goal [id=" + id + ", name=" + name + ", description=" + description + ", image=" + image + ", date="
+				+ date + ", currentAmount=" + currentAmount + ", totalAmount=" + totalAmount + ", user=" + user
+				+ "]";
+	}	
+}
---
+++ savings-api/src/main/java/com/skillstorm/savingsapi/models/User.java
@@ -0,0 +1,110 @@
+package com.skillstorm.savingsapi.models;
+
+import java.util.*;
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userID;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "firstName")
+	private String firstName;
+	
+	@Column(name = "lastName")
+	private String lastName;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonManagedReference
+	private Set<Goal> goals;
+
+	// no-argument constructor
+	public User() {
+		super();
+	}
+
+	// constructor without the auto-incremented id
+	public User(String username, String password, String firstName, String lastName) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	// full-argument constructor
+	public User(int userID, String username, String password, String firstName, String lastName) {
+		super();
+		this.userID = userID;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	// getters and setters
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Set<Goal> getGoals() {
+		return goals;
+	}
+
+	public void setGoals(Set<Goal> goals) {
+		this.goals = goals;
+	}
+
+	// to-string function
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", username=" + username + ", password=" + password + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", goals=" + goals + "]";
+	}
+}
