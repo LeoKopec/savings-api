9ccbb6b8940fb1079fb80e1ab346dbdfd36ad8d9
Leo-Kopec <lkopec4@gmail.com>
2022-08-31 11:16:19 -0400

    Added delete to User Controller

--- savings-api/src/main/java/com/skillstorm/savingsapi/controllers/UserController.java
+++ savings-api/src/main/java/com/skillstorm/savingsapi/controllers/UserController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,12 @@ public class UserController {
 	public User update(@Valid @RequestBody User user, @PathVariable int id) {
 		user.setUser_id(id);
 		return service.update(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable int id) {
+		service.deleteById(id);
 	}
 
 }
