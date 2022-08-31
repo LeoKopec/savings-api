bc5ab5b3c016d81a48d343c6c211801520ada326
Leo-Kopec <lkopec4@gmail.com>
2022-08-31 09:59:54 -0400

    Added findById to Goal Controller

--- savings-api/src/main/java/com/skillstorm/savingsapi/controllers/GoalController.java
+++ savings-api/src/main/java/com/skillstorm/savingsapi/controllers/GoalController.java
@@ -1,6 +1,8 @@
 package com.skillstorm.savingsapi.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,5 +21,11 @@ public class GoalController {
 	@ResponseStatus(code = HttpStatus.OK)
 	public List<Goal> findByUsername(@PathVariable String username) {
 		return service.findByUsername(username);
+	}
+	
+	@GetMapping("/{username}/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public Optional<Goal> findById(@PathVariable int id) {
+		return service.findById(id);
 	}
 }
