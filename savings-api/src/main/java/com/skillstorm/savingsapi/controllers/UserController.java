d3551be3b2142ef8f4accfeba4459e99b6ed431d
Leo-Kopec <lkopec4@gmail.com>
2022-08-31 11:08:47 -0400

    Added create to User Controller

--- savings-api/src/main/java/com/skillstorm/savingsapi/controllers/UserController.java
+++ savings-api/src/main/java/com/skillstorm/savingsapi/controllers/UserController.java
@@ -2,11 +2,15 @@ package com.skillstorm.savingsapi.controllers;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +35,12 @@ public class UserController {
 	@ResponseStatus(code = HttpStatus.OK)
 	public Optional<User> findById(@PathVariable int id) {
 		return service.findById(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public User create(@Valid @RequestBody User user) {
+		return service.save(user);
 	}
 
 }
